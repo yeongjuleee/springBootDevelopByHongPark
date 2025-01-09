418097e9ce02b0b0e2cd333bc376c196d0218bb8
vkslvkd092 <vkslvkd092@naver.com>
2025-01-09 15:40:26 +0900

    Merge changes from origin/main

---
+++ src/main/java/com/example/firstproject/HongParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongParkApplication.class, args);
+    }
+
+}
