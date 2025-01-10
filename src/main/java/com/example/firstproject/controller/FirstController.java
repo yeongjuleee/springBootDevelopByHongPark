c9ba4dd7a7636202d47f68942321a9c07fd1f154
vkslvkd092 <vkslvkd092@naver.com>
2025-01-10 17:30:38 +0900

    feat(controller) : Add FirstController and greetings.mustache
    docs(README) : Upate project setup instructions

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,11 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+
+    public String niceToMeetYou() {
+        return "";
+    }
+}
