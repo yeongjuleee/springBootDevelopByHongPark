4b2efc67b0df80aa5dabd5fb7981151d2ab24177
vkslvkd092 <vkslvkd092@naver.com>
2025-01-10 21:38:33 +0900

    feat(controller): Add 'hi' endpoint
    docs(application.properties): Add encoding configuration to prevent Korean text corruption

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,11 +1,13 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
+    @GetMapping("/hi") // URL 요청 접수. 이것이 없으면 URL을 찾을 수 없어서 404가 나옴
     public String niceToMeetYou() {
-        return "";
+        return "greetings";
     }
 }
