47edea8268bca936c05a260c4ecf8c8629134c96
vkslvkd092 <vkslvkd092@naver.com>
2025-01-13 23:02:44 +0900

    commit

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
