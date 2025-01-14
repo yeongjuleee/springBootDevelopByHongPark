0af2cc7d72a1f9e8abcc542e2628441e3821ef28
vkslvkd092 <vkslvkd092@naver.com>
2025-01-14 21:51:39 +0900

    feat(MemberRepository) : Override `findAll()` method and change return type from `Iterable` to `ArrayList`

--- src/main/java/com/example/firstproject/repository/MemberRepository.java
+++ src/main/java/com/example/firstproject/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.firstproject.repository;
 import com.example.firstproject.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll(); // Override : 상속 받은 메서드를 자식 클래스에서 재정의하여 사용하는 것
 }
